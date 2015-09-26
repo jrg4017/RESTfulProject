d0c704e2d89c4ffd0a9c5d6d67a8570a9f3f9bde
jrg4017 <jrg4017@rit.edu>
2015-09-25 21:09:26 -0400

    RESTful site

---
+++ assets/includes/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>ISTE340 P2</title>
+    <script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+
+    <!-- link to css main -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <!-- JQUERY PLUGIN 1: show / hide search box -->
+    <script src="assets/js/showHide.js"></script>
+
+    <!-- JQUERY PLUGIN 2: POP UP WITH TABS -->
+    <script src="http://code.jboxcdn.com/0.3.2/jBox.min.js"></script>
+    <link href="http://code.jboxcdn.com/0.3.2/jBox.css" rel="stylesheet">
+
+    <!--JQUERY PLUGIN 3: TABS WITH DATA-->
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+    <!--JQUERY PLUGIN 4: PAGINATION WITH CONTROL OVER RESULTS-->
+    <script src="//cdn.datatables.net/1.10.6/js/jquery.dataTables.min.js"></script>
+    <link href="assets/css/datatable.css" rel="stylesheet">
+
+    <!--JQUERY PLUGIN 5: GOOGLE MAPS-->
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyAxaRnS-0a3lXGcPXW_XPd43ROMGBN8jaI&sensor=false"></script>
+
+
+    <!--PURE CSS styles -->
+    <link rel="stylesheet" href="assets/css/pure.css">
+
+    <script type="text/javascript">
